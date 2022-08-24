58066463985d862484b03e4ac8c02db3e036dc36
Danik <chapagana@gmail.com>
2022-08-24 19:04:50 +0600

    Add create room view

--- api/serializers.py
+++ api/serializers.py
@@ -6,3 +6,10 @@ class RoomSerializer(serializers.ModelSerializer):
     class Meta:
         model = Room
         fields = "__all__"
+
+
+# this serializer is for making sure post request contains correct data
+class CreateRoomSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Room
+        fields = ("guest_can_pause", "votes_to_skip")
